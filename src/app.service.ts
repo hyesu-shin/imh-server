72f0cb52cdbdc1d1c7ca2fc6a30dc0e30b3d7ad6
hyesu <didimxmg08@gmail.com>
2025-02-04 20:11:51 +0900

    refactor: initialize Nest

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  constructor (private readonly configService: ConfigService) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
